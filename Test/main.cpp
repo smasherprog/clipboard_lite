8dd2fa3efe503678b9569b006a6aaa5e6543441e
scott <smasherprog@gmail.com>
2017-06-26 20:34:18 -0700

    adding linux implementation

--- Test/main.cpp
+++ Test/main.cpp
@@ -11,21 +11,14 @@ int main(int argc, char* argv[]) {
     bool done = false;
     std::cout << "The example will exit if you copy or cut rtf to the clipboard" << std::endl;
     auto clipboard = SL::Clipboard_Lite::CreateClipboard()
-        .onHTML([](const std::string& html) {
-        std::cout << "onHTML" << std::endl;
-        std::cout << html << std::endl;
-    }).onRTF([&](const std::string& rtf) {
-        std::cout << "onRTF" << std::endl;
-        std::cout << rtf << std::endl;
-        done = true;
-    }).onText([](const std::string& text) {
+    .onText([](const std::string& text) {
         std::cout << "onText" << std::endl;
         std::cout << text << std::endl;
     }).run();
 
     std::this_thread::sleep_for(1s);
     std::string txt = "pasted text";
-    clipboard.paste_Text(txt);
+    clipboard.paste(txt);
 
     while (!done) {
         std::this_thread::sleep_for(1s);
--- src/Clipboard_Lite.cpp
+++ src/Clipboard_Lite.cpp
@@ -7,27 +7,10 @@
 namespace SL {
     namespace Clipboard_Lite {
 
-        void Clipboard_Manager::paste_HTML(const std::string& html) {
-            Impl_->paste_HTML(html);
+        void Clipboard_Manager::paste(const std::string& text) {
+            Impl_->paste(text);
         }
-        void Clipboard_Manager::paste_RTF(const std::string& rtf) {
-            Impl_->paste_RTF(rtf);
-        }
-        void Clipboard_Manager::paste_Text(const std::string& text) {
-            Impl_->paste_Text(text);
-        }
-    
-        Clipboard_Configuration Clipboard_Configuration::onHTML(const std::function<void(const std::string& html)>& handle) {
-            assert(!Impl_->onHTML);
-            Impl_->onHTML = handle;
-            return Clipboard_Configuration(Impl_);
 
-        }
-        Clipboard_Configuration Clipboard_Configuration::onRTF(const std::function<void(const std::string& rtf)>& handle) {
-            assert(!Impl_->onRTF);
-            Impl_->onRTF = handle;
-            return Clipboard_Configuration(Impl_);
-        }
         Clipboard_Configuration Clipboard_Configuration::onText(const std::function<void(const std::string& text)>& handle) {
             assert(!Impl_->onText);
             Impl_->onText = handle;
