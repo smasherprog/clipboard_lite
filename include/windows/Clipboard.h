3f1bea0262ce9d18d8383e0f9de61cbed8f5a80e
Scott <smasherprog@gmail.com>
2017-06-26 20:40:48 -0700

    fixed windows build changed to just text

--- include/windows/Clipboard.h
+++ include/windows/Clipboard.h
@@ -18,17 +18,52 @@ namespace SL {
             HWND Hwnd = nullptr;
         public:
 
-            std::function<void(const std::string& html)> onHTML;
-            std::function<void(const std::string& html)> onRTF;
             std::function<void(const std::string& text)> onText;
 
             Clipboard_ManagerImpl();
             ~Clipboard_ManagerImpl();
 
             void run();
-            void paste_HTML(const std::string& html);
-            void paste_RTF(const std::string& rtf);
-            void paste_Text(const std::string& text);
+            void paste(const std::string& test);
+
+            template<class T>bool LoadClip(T& buffer, UINT format) {
+                if (IsClipboardFormatAvailable(format)) {
+                    auto h = ::GetClipboardData(format);
+                    if (h) {
+                        auto pData = GlobalLock(h);
+                        auto nLength = GlobalSize(h);
+                        if (pData && nLength > 0) {
+                            buffer.resize(nLength);
+                            memcpy((void*)buffer.data(), pData, nLength);
+                            GlobalUnlock(h);
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+            template<class T>void RestoreClip(const T& buffer, UINT format) {
+                if (OpenClipboard(Hwnd) == TRUE) {
+                    if (EmptyClipboard() == TRUE && buffer.size() > 0) {
+                        auto hData = GlobalAlloc(GMEM_MOVEABLE, buffer.size());
+                        if (hData) {
+                            auto pData = GlobalLock(hData);
+                            if (pData) {
+                                memcpy(pData, buffer.data(), buffer.size());
+                                GlobalUnlock(hData);
+                                if (::SetClipboardData(format, hData)) {
+                                    //clipboard takes ownership of the memory
+                                    hData = nullptr;
+                                }
+                            }
+                        }
+                        if (hData) {
+                            GlobalFree(hData);
+                        }
+                    }
+                    CloseClipboard();
+                }
+            }
         };
     }
 }
--- src/windows/Clipboard.cpp
+++ src/windows/Clipboard.cpp
@@ -5,46 +5,7 @@
 namespace SL {
     namespace Clipboard_Lite {
 
-        const UINT formatRTF = RegisterClipboardFormat(_T("Rich Text Format"));
-        const UINT formatHTML = RegisterClipboardFormat(_T("HTML Format"));
-        template<class T>bool LoadClip(T& buffer, UINT format) {
-            if (IsClipboardFormatAvailable(format)) {
-                auto h = ::GetClipboardData(format);
-                if (h) {
-                    auto pData = GlobalLock(h);
-                    auto nLength = GlobalSize(h);
-                    if (pData && nLength > 0) {
-                        buffer.resize(nLength);
-                        memcpy((void*)buffer.data(), pData, nLength);
-                        GlobalUnlock(h);
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
-        template<class T>void RestoreClip(const T& buffer, UINT format) {
-            if (OpenClipboard(Hwnd) == TRUE) {
-                if (EmptyClipboard() == TRUE && buffer.size() > 0) {
-                    auto hData = GlobalAlloc(GMEM_MOVEABLE, buffer.size());
-                    if (hData) {
-                        auto pData = GlobalLock(hData);
-                        if (pData) {
-                            memcpy(pData, buffer.data(), buffer.size());
-                            GlobalUnlock(hData);
-                            if (::SetClipboardData(format, hData)) {
-                                //clipboard takes ownership of the memory
-                                hData = nullptr;
-                            }
-                        }
-                    }
-                    if (hData) {
-                        GlobalFree(hData);
-                    }
-                }
-                CloseClipboard();
-            }
-        }
+
         Clipboard_ManagerImpl::Clipboard_ManagerImpl() {
             Pasting = false;
         }
@@ -73,13 +34,7 @@ namespace SL {
                                 if (msg.message == WM_CLIPBOARDUPDATE) {
                                     if (!Pasting && OpenClipboard(Hwnd) == TRUE) {
                                         std::string data;
-                                        if (onRTF && LoadClip(data, formatRTF)) {
-                                            onRTF(data);
-                                        }
-                                        else if (onHTML && LoadClip(data, formatHTML)) {
-                                            onHTML(data);
-                                        }
-                                        else if (onText && LoadClip(data, CF_TEXT)) {
+                                        if (onText && LoadClip(data, CF_TEXT)) {
                                             onText(data);
                                         }
                                         CloseClipboard();
@@ -99,15 +54,8 @@ namespace SL {
                 }
             });
         }
-        void Clipboard_ManagerImpl::paste_HTML(const std::string& html) {
-            Pasting = true;
-            RestoreClip(html, formatHTML);
-        }
-        void Clipboard_ManagerImpl::paste_RTF(const std::string& rtf) {
-            Pasting = true;
-            RestoreClip(rtf, formatRTF);
-        }
-        void Clipboard_ManagerImpl::paste_Text(const std::string& text) {
+
+        void Clipboard_ManagerImpl::paste(const std::string& text) {
             Pasting = true;
             RestoreClip(text, CF_TEXT);
         }
