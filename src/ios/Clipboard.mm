7717372dae09a7673652d02994895ef8ead5f3d0
scott lee <smasherprog@gmail.com>
2017-07-29 15:25:53 -0700

    Added mac onText event callback

--- src/ios/Clipboard.mm
+++ src/ios/Clipboard.mm
@@ -9,6 +9,7 @@
 #include <memory>
 #include <iostream>
 #include <assert.h>
+#include <chrono>
 
 namespace SL {
     namespace Clipboard_Lite {
@@ -16,14 +17,40 @@ namespace SL {
             Copying = false;
         }
         Clipboard_ManagerImpl::~Clipboard_ManagerImpl() {
-            
+            KeepRunning = false;
             if (BackGroundWorker.joinable()) {
                 BackGroundWorker.join();
             }
         }
+        std::string	convertNsString( NSString *str )
+        {
+            return std::string( [str UTF8String] );
+        }
         void Clipboard_ManagerImpl::run() {
+            KeepRunning = true;
             BackGroundWorker = std::thread([&] {
-                
+                //mac platform we must poll to retrive clipboard events
+                while(KeepRunning){
+                    NSPasteboard *pasteboard = [NSPasteboard generalPasteboard];
+                    if(pasteboard.changeCount != ChangeCount){
+                        ChangeCount = pasteboard.changeCount;
+                        if(onText){
+                            
+                        NSArray *classArray = [NSArray arrayWithObject:[NSString class]];
+                        NSDictionary *options = [NSDictionary dictionary];
+                    
+                        if( [pasteboard canReadObjectForClasses:classArray options:options] ) {
+                            NSArray *objectsToPaste = [pasteboard readObjectsForClasses:classArray options:options];
+                            NSString *text = [objectsToPaste firstObject];
+                            if(text){
+                                onText(convertNsString(text));
+                            }
+                        }
+                        }
+                    }
+                   
+                    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+                }
             });
         }
         void Clipboard_ManagerImpl::copy(const std::string& text) {
